5a81e65248c2efe3606160f3960e9897014fcb5a
W0dl <icetwisted80@gmail.com>
2024-12-29 14:01:41 +0100

    Delete plug-studios-main directory

--- plug-studios-main/src/lib/Icons.ts
+++
@@ -1,7 +0,0 @@
-// @ts-nocheck
-
-import MdiHumanWelcome from "~icons/mdi/human-welcome";
-import IcOutlineMessage from "~icons/ic/outline-message";
-import IconParkOutlineUser from "~icons/icon-park-outline/user";
-
-export { MdiHumanWelcome, IcOutlineMessage, IconParkOutlineUser };
